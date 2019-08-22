a12edf3d3aef815240ca21cdd59ef9e99cb344c7
luisaga215 <luis.aga215@gmail.com>
2019-08-22 02:45:03 -0500

    navbar

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 
 app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/assets'));
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
